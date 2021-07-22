6a432ad67a5b842e1590994442285c733ba362ea
DeviArya <aryadevivdev@gmail.com>
2021-07-22 08:29:15 +0530

    taskify

---
+++ TaskManager_App/app/src/main/java/com/example/taskmanager_app/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.taskmanager_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+ TextView mtvSplash;
+ ImageView mIvSplash;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mtvSplash=findViewById(R.id.TextViewSplashScreen);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
+        mtvSplash.setAnimation(animation);
+        mtvSplash.startAnimation(animation);
+
+        mIvSplash=findViewById(R.id.imageViewSplashScreen);
+        Animation animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce);
+        mIvSplash.setAnimation(animation1);
+        mIvSplash.startAnimation(animation1);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+            }
+        },5000);
+    }
+}
