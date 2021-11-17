06cb2a5717716d07fcb20d0a45f769209e084eda
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-11-17 13:26:50 +0530

    colors.xml added.

---
+++ Taskify/app/src/main/java/kunal/project/taskify/SplashActivity.java
@@ -0,0 +1,14 @@
+package kunal.project.taskify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
